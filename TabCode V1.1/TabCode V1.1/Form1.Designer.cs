f5c0e75fec2892949defdb449104f4f9755fa3a7
İbrahim <87008174+IibrahimEren@users.noreply.github.com>
2023-03-17 16:43:50 +0300

    Add files via upload

---
+++ TabCode V1.1/TabCode V1.1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace TabCode_V1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnCevir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHarfler = new System.Windows.Forms.TextBox();
+            this.txtRakamlar = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.Location = new System.Drawing.Point(2, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(674, 449);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(666, 416);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Harflerden Rakamlara";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(11, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Çevirmek istediğiniz cümleyi girin:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox2.Location = new System.Drawing.Point(16, 226);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(624, 182);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(625, 148);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage2.Controls.Add(this.btnCevir);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.txtHarfler);
+            this.tabPage2.Controls.Add(this.txtRakamlar);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(666, 416);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Rakamlardan Harflere";
+            // 
+            // btnCevir
+            // 
+            this.btnCevir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCevir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCevir.ForeColor = System.Drawing.Color.White;
+            this.btnCevir.Image = global::TabCode_V1._1.Properties.Resources.Başlıksız_formAç;
+            this.btnCevir.Location = new System.Drawing.Point(506, 186);
+            this.btnCevir.Name = "btnCevir";
+            this.btnCevir.Size = new System.Drawing.Size(134, 34);
+            this.btnCevir.TabIndex = 5;
+            this.btnCevir.Text = "Çevir";
+            this.btnCevir.UseVisualStyleBackColor = true;
+            this.btnCevir.Click += new System.EventHandler(this.btnCevir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(11, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Çevirmek istediğiniz rakamları girin:";
+            // 
+            // txtHarfler
+            // 
+            this.txtHarfler.BackColor = System.Drawing.Color.DarkGray;
+            this.txtHarfler.Location = new System.Drawing.Point(16, 226);
+            this.txtHarfler.Multiline = true;
+            this.txtHarfler.Name = "txtHarfler";
+            this.txtHarfler.ReadOnly = true;
+            this.txtHarfler.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHarfler.Size = new System.Drawing.Size(624, 182);
+            this.txtHarfler.TabIndex = 1;
+            // 
+            // txtRakamlar
+            // 
+            this.txtRakamlar.Location = new System.Drawing.Point(15, 32);
+            this.txtRakamlar.Multiline = true;
+            this.txtRakamlar.Name = "txtRakamlar";
+            this.txtRakamlar.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRakamlar.Size = new System.Drawing.Size(625, 148);
+            this.txtRakamlar.TabIndex = 0;
+            this.txtRakamlar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRakamlar_KeyPress);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TabCode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCevir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHarfler;
+        private System.Windows.Forms.TextBox txtRakamlar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ TabCode V1.1/TabCode V1.1/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabCode_V1._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region HARFTEN RAKAMLARA BAŞLANGIÇ
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsSeparator(e.KeyChar) &&
+                (e.KeyChar != '.' && e.KeyChar != ',' && e.KeyChar != '?' && e.KeyChar != '!'))
+                e.Handled = true;
+            if ((e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1))
+                e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            char[] Harfler;
+            Harfler = textBox1.Text.ToArray();
+            string cevrilmişifade = "";
+            foreach (var item in Harfler)
+            {
+                try
+                {
+                    switch (item)
+                    {
+                        case 'A':
+                        case 'a':
+                            cevrilmişifade += "" + "11";
+                            break;
+                        case 'B':
+                        case 'b':
+                            cevrilmişifade += "" + "12";
+                            break;
+                        case 'C':
+                        case 'c':
+                            cevrilmişifade += "" + "13";
+                            break;
+                            break;
+                        case 'D':
+                        case 'd':
+                            cevrilmişifade += "" + "14";
+                            break;
+                        case 'E':
+                        case 'e':
+                            cevrilmişifade += "" + "15";
+                            break;
+                        case 'F':
+                        case 'f':
+                            cevrilmişifade += "" + "21";
+                            break;
+                        case 'G':
+                        case 'g':
+                            cevrilmişifade += "" + "22";
+                            break;
+                        case 'H':
+                        case 'h':
+                            cevrilmişifade += "" + "23";
+                            break;
+                        case 'I':
+                        case 'i':
+                            cevrilmişifade += "" + "24";
+                            break;
+                        case 'J':
+                        case 'j':
+                            cevrilmişifade += "" + "25";
+                            break;
+                        case 'L':
+                        case 'l':
+                            cevrilmişifade += "" + "31";
+                            break;
+                        case 'M':
+                        case 'm':
+                            cevrilmişifade += "" + "32";
+                            break;
+                        case 'N':
+                        case 'n':
+                            cevrilmişifade += "" + "33";
+                            break;
+                        case 'O':
+                        case 'o':
+                            cevrilmişifade += "" + "34";
+                            break;
+                        case 'P':
+                        case 'p':
+                            cevrilmişifade += "" + "35";
+                            break;
+                        case 'Q':
+                        case 'q':
+                            cevrilmişifade += "" + "41";
+                            break;
+                        case 'R':
+                        case 'r':
+                            cevrilmişifade += "" + "42";
+                            break;
+                        case 'S':
+                        case 's':
+                            cevrilmişifade += "" + "43";
+                            break;
+                        case 'T':
+                        case 't':
+                            cevrilmişifade += "" + "44";
+                            break;
+                        case 'U':
+                        case 'u':
+                            cevrilmişifade += "" + "45";
+                            break;
+                        case 'V':
+                        case 'v':
+                            cevrilmişifade += "" + "51";
+                            break;
+                        case 'Y':
+                        case 'y':
+                            cevrilmişifade += "" + "54";
+                            break;
+                        case 'W':
+                        case 'w':
+                            cevrilmişifade += "" + "52";
+                            break;
+                        case 'X':
+                        case 'x':
+                            cevrilmişifade += "" + "53";
+                            break;
+                        case 'Z':
+                        case 'z':
+                            cevrilmişifade += "" + "55";
+                            break;
+                        case 'K':
+                        case 'k':
+                            cevrilmişifade += "61";
+                            break;
+                        case 'ı':
+                            cevrilmişifade += "16";
+                            break;
+                        case 'Ö':
+                        case 'ö':
+                            cevrilmişifade += "64";
+                            break;
+                        case 'Ü':
+                        case 'ü':
+                            cevrilmişifade += "63";
+                            break;
+                        case 'Ç':
+                        case 'ç':
+                            cevrilmişifade += "65";
+                            break;
+                        case 'Ğ':
+                        case 'ğ':
+                            cevrilmişifade += "62";
+                            break;
+                        case 'Ş':
+                        case 'ş':
+                            cevrilmişifade += "26";
+                            break;
+                        case ' ':
+                            cevrilmişifade += "" + " ";
+                            break;
+                        case '.':
+                            cevrilmişifade += "36";
+                            break;
+                        case ',':
+                            cevrilmişifade += "46";
+                            break;
+                        case '?':
+                            cevrilmişifade += "56";
+                            break;
+                        case '!':
+                            cevrilmişifade += "66";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    textBox1.Focus();
+                }
+            }
+            textBox2.Text = (cevrilmişifade);
+        }
+        #endregion HARFTEN RAKAMLARA SON
+        #region RAKAMLARDAN HARFLERE  BAŞLANGIÇ
+        private void txtRakamlar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.' && e.KeyChar != ',' && e.KeyChar != '?' && e.KeyChar != '!'))
+                e.Handled = true;
+            if ((e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1))
+                e.Handled = true;
+        }
+
+        private void btnCevir_Click(object sender, EventArgs e)
+        {
+            char[] rakam;
+            rakam = txtRakamlar.Text.ToArray();
+            string harfler = "";
+            if (txtRakamlar.Text == "")
+                errorProvider1.SetError(txtRakamlar, "Boş bırakmamayı unutmayın"); 
+            for (int i = 0; i < rakam.Length; i++)
+            {
+                if (i + 1 == rakam.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    if (rakam[i].ToString() == "1")
+                    {
+                        if (rakam[i + 1].ToString() == "1")
+                        {
+                            harfler += "A";
+                        }
+                        else if (rakam[i + 1].ToString() == "2")
+                        {
+                            harfler += "B";
+                        }
+                        else if (rakam[i + 1].ToString() == "3")
+                        {
+                            harfler += "C";
+                        }
+                        else if (rakam[i + 1].ToString() == "4")
+                        {
+                            harfler += "D";
+                        }
+                        else if (rakam[i + 1].ToString() == "5")
+                        {
+                            harfler += "E";
+                        }
+                        else if (rakam[i + 1].ToString() == "6")
+                            harfler += "I";
+                        else
+                            harfler += "*";
+                    }
+                    else if (rakam[i].ToString() == "2")
+                    {
+                        if (rakam[i + 1].ToString() == "1")
+                        {
+                            harfler += "F";
+                        }
+                        else if (rakam[i + 1].ToString() == "2")
+                        {
+                            harfler += "G";
+                        }
+                        else if (rakam[i + 1].ToString() == "3")
+                        {
+                            harfler += "H";
+                        }
+                        else if (rakam[i + 1].ToString() == "4")
+                        {
+                            harfler += "I";
+                        }
+                        else if (rakam[i + 1].ToString() == "5")
+                        {
+                            harfler += "J";
+                        }
+                        else if (rakam[i + 1].ToString() == "6")
+                            harfler += "Ş";
+                        else
+                            harfler += "*";
+                    }
+                    else if (rakam[i].ToString() == "3")
+                    {
+                        if (rakam[i + 1].ToString() == "1")
+                        {
+                            harfler += "L";
+                        }
+                        else if (rakam[i + 1].ToString() == "2")
+                        {
+                            harfler += "M";
+                        }
+                        else if (rakam[i + 1].ToString() == "3")
+                        {
+                            harfler += "N";
+                        }
+                        else if (rakam[i + 1].ToString() == "4")
+                        {
+                            harfler += "O";
+                        }
+                        else if (rakam[i + 1].ToString() == "5")
+                        {
+                            harfler += "P";
+                        }
+                        else if (rakam[i + 1].ToString() == "6")
+                            harfler += ".";
+                        else
+                            harfler += "*";
+                    }
+                    else if (rakam[i].ToString() == "4")
+                    {
+                        if (rakam[i + 1].ToString() == "1")
+                        {
+                            harfler += "Q";
+                        }
+                        else if (rakam[i + 1].ToString() == "2")
+                        {
+                            harfler += "R";
+                        }
+                        else if (rakam[i + 1].ToString() == "3")
+                        {
+                            harfler += "S";
+                        }
+                        else if (rakam[i + 1].ToString() == "4")
+                        {
+                            harfler += "T";
+                        }
+                        else if (rakam[i + 1].ToString() == "5")
+                        {
+                            harfler += "U";
+                        }
+                        else if (rakam[i + 1].ToString() == "6")
+                            harfler += ",";
+                        else
+                            harfler += "*";
+                    }
+                    else if (rakam[i].ToString() == "5")
+                    {
+                        if (rakam[i + 1].ToString() == "1")
+                        {
+                            harfler += "V";
+                        }
+                        else if (rakam[i + 1].ToString() == "2")
+                        {
+                            harfler += "W";
+                        }
+                        else if (rakam[i + 1].ToString() == "3")
+                        {
+                            harfler += "X";
+                        }
+                        else if (rakam[i + 1].ToString() == "4")
+                        {
+                            harfler += "Y";
+                        }
+                        else if (rakam[i + 1].ToString() == "5")
+                        {
+                            harfler += "Z";
+                        }
+                        else if (rakam[i + 1].ToString() == "6")
+                            harfler += "?";
+                        else
+                            harfler += "*";
+                    }
+                    //tamam burasını ilerleticem ya
+                    else if (rakam[i].ToString() == "6")
+                    {
+                        if (rakam[i + 1].ToString() == "1")
+                            harfler += "K";
+                        else if (rakam[i + 1].ToString() == "2")
+                            harfler += "Ğ";
+                        else if (rakam[i + 1].ToString() == "3")
+                            harfler += "Ü";
+                        else if (rakam[i + 1].ToString() == "4")
+                            harfler += "Ö";
+                        else if (rakam[i + 1].ToString() == "5")
+                            harfler += "Ç";
+                        else if (rakam[i + 1].ToString() == "6")
+                            harfler += "!";
+                    }
+                    else if (rakam[i].ToString() == ",")
+                        harfler += ",";
+                    else if (rakam[i].ToString() == ".")
+                        harfler += ".";
+                    else if (rakam[i].ToString() == "?")
+                        harfler += "?";
+                    else if (rakam[i].ToString() == "!")
+                        harfler += "!";
+                    else if (rakam[i].ToString() == " ")
+                    {
+                        harfler += " ";
+                        continue;
+                    }
+
+                    else
+                        harfler += "*";
+
+                    i++;
+                }
+
+            }
+            txtHarfler.Text = harfler;
+        }
+        #endregion RAKAMLARDAN HARFLERE SON
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AcceptButton = btnCevir;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            textBox1.Focus();
+        }
+    }
+}
